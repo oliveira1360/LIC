83b97adf045096d18d900ac9da84426e03880708
Francisco Prieto <francisco.marques.prieto@gmail.com>
2024-03-12 12:19:56 +0000

    several changes

--- src/HAL.kt
+++ src/HAL.kt
@@ -1,20 +1,16 @@
 import isel.leic.UsbPort
 
 object HAL { // Virtualiza o acesso ao sistema UsbPort
-
+    var plate_value : Int = 0
 
     // Inicia a classe
     fun init(){
-
+        plate_value = 0b0000
+        UsbPort.write(plate_value)
     }
 
-
-
     // Retorna true se o bit tiver o valor lógico ‘1’
-
-
-
-
+    fun isBit(mask: Int) : Boolean = mask == UsbPort.read()
 
     // Retorna os valores dos bits representados por mask presentes no UsbPort
     fun readBits(mask: Int): Int {
@@ -22,26 +18,24 @@ object HAL { // Virtualiza o acesso ao sistema UsbPort
         return values.and(mask)
     }
 
-
-
-
-
     // Escreve nos bits representados por mask os valores dos bits correspondentes em value
     fun writeBits(mask: Int, value: Int) {
-
+        mask.and(value)
+        plate_value = plate_value.or(mask)
+        UsbPort.write(plate_value)
     }
-
-
 
     // Coloca os bits representados por mask no valor lógico ‘1’
     fun setBits(mask: Int) {
-
+        val valor = plate_value.or(mask)
+        plate_value = valor
+        UsbPort.write(plate_value)
     }
-
-
 
     // Coloca os bits representados por mask no valor lógico ‘0’
     fun clrBits(mask: Int) {
-
+        val valor = plate_value.and(mask.inv())
+        plate_value = valor
+        UsbPort.write(plate_value)
     }
 }
--- src/Main.kt
+++ src/Main.kt
@@ -2,8 +2,12 @@ import isel.leic.UsbPort
 
 fun main() {
     while (true) {
+        HAL.init()
         Thread.sleep(1500);
-        println(HAL.readBits(101))
+        //println(HAL.readBits(0b0101))
+        println(HAL.plate_value)
+       //HAL.writeBits(0b0101, 0b0101)
+        HAL.setBits(0b0001)
     }
 
 }

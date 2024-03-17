6018a9688cc89b8958b572d74894a92ff5d40abc
oliveira1360 <diogo.oliveira.do17@gmail.com>
2024-03-17 19:42:13 +0000

    kgb

--- src/KBD.kt
+++ src/KBD.kt
@@ -1,6 +1,4 @@
-
-
-
+import isel.leic.UsbPort
 
 
 object KBD { // Ler teclas. Métodos retornam ‘0’..’9’,’#’,’*’ ou NONE.
@@ -15,12 +13,15 @@ object KBD { // Ler teclas. Métodos retornam ‘0’..’9’,’#’,’*’ o
 
     // Retorna de imediato a tecla premida ou NONE se não há tecla premida.
     fun getKey(): Char {
-        return 's'
+        val tecla = UsbPort.keyreader()
+
+        return if (tecla != 0) tecla.toChar() else ' '
     }
 
 
     // Retorna a tecla premida, caso ocorra antes do ‘timeout’ (representado em milissegundos), ou NONE caso contrário.
     fun waitKey(timeout: Long): Char {
-        return  'a'
+        val tecla = UsbPort.keyreader()
+        return if (timeout < tecla) tecla else ' '
     }
 }

fd8ff2e6e63ff70fe3eeeaf1d920c833816cfb41
Francisco Prieto <francisco.marques.prieto@gmail.com>
2024-03-19 08:20:21 +0000

    some changes

--- src/KBD.kt
+++ src/KBD.kt
@@ -1,31 +1,26 @@
 import isel.leic.UsbPort
 import isel.leic.utils.KeyReader
-
-
-
+import isel.leic.utils.Time
 
 object KBD { // Ler teclas. Métodos retornam ‘0’..’9’,’#’,’*’ ou NONE.
     const val NONE = 0;
-
 
     // Inicia a classe
     fun init() {
 
     }
 
-
     // Retorna de imediato a tecla premida ou NONE se não há tecla premida.
     fun getKey(): Char {
         val tecla = KeyReader().waitKeyChar()
-
-        return if (tecla == null ) ' ' else tecla
+        return if (KeyReader().isAnyKeyPressed ) tecla else ' '
     }
-
 
     // Retorna a tecla premida, caso ocorra antes do ‘timeout’ (representado em milissegundos), ou NONE caso contrário.
     fun waitKey(timeout: Long): Char {
-       val valorAntesDoTimeOut = 0.0.toLong()
-        val tecla = KeyReader().waitKeyChar()
-        return if (timeout < valorAntesDoTimeOut) tecla else ' '
+       //val valorAntesDoTimeOut = 0.0.toLong()
+        val time  = Time.getTimeInMillis()
+        val tecla = getKey()
+        return if (time < timeout) tecla else ' '
     }
 }

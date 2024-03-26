3ce1baf70f71fac4e7aed6f37ac3de871ac078dd
oliveira1360 <diogo.oliveira.do17@gmail.com>
2024-03-26 14:37:49 +0000

    isBit change

--- src/KBD.kt
+++ src/KBD.kt
@@ -2,11 +2,21 @@
 import isel.leic.UsbPort
 import isel.leic.utils.Time
 
+fun main() {
+
+    //LCD.writeByteParallel(false,0b0011_0000)
+    HAL.init()
+    while (true)
+        println( KBD.getKey())
+}
+
+
 object KBD { // Ler teclas. Métodos retornam ‘0’..’9’,’#’,’*’ ou NONE.
     const val NONE = 0;
     const val CLR_ALL_BITS = 0b1111_1111
     const val SET_ACK_1 = 0b0001_0000
-    const val CHECKBIT = 0b0001_0000
+    //    const val CHECKBIT = 0b0001_0000
+    const val CHECKBIT = 0b0000_0001
     private val digitArray = "147*2580369#    ".toCharArray()
     const val NONERETURN = ' '
 
@@ -18,10 +28,11 @@ object KBD { // Ler teclas. Métodos retornam ‘0’..’9’,’#’,’*’ o
     // Retorna de imediato a tecla premida ou NONE se não há tecla premida.
     fun getKey(): Char {
         if (HAL.isBit(CHECKBIT) ) {
-                val tecla = HAL.readBits(0b0000_1111)
-                HAL.setBits(SET_ACK_1)
-                HAL.clrBits(CLR_ALL_BITS)
-                return digitArray[tecla]
+            //val tecla = HAL.readBits(0b0000_1111)
+            val tecla = HAL.readBits(0b0001_1110).shr(1)
+            HAL.setBits(SET_ACK_1)
+            HAL.clrBits(SET_ACK_1)
+            return digitArray[tecla]
         }
         return NONERETURN
     }

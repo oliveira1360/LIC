ed0840a96c557d3a08c7b5f393e0e5cbd1d0fd9a
oliveira1360 <diogo.oliveira.do17@gmail.com>
2024-05-16 21:39:08 +0100

    need to adjsut de LCD too kill moster better

--- src/Main.kt
+++ src/Main.kt
@@ -1,62 +1,150 @@
-import isel.leic.UsbPort
 import isel.leic.utils.Time
 
-
-const val SCORE_INCREASE = 1
 const val MOSTER_SPAW_LINE_1_COORDENATES = 20
 const val MOSTER_SPAW_LINE_2_COORDENATES = 7
 const val digitArray = "1472580369"
+const val TIME_TO_SPAWN = 1800L
+var posMain = false
+var last = 0
+
 
 fun main() {
-        var Linah1 = ""
-        var Linah2 = ""
-        var Pos1 = 15
-        var Pos2 = 15
+        //inicilizacao da main
         HAL.init()
         LCD.init()
         KBD.init()
         SerialEmitter.init()
         SerialEmitter.init()
 
+        var linha0 = ""
+        var linha1 = ""
+        var Pos1 = 15
+        var Pos2 = 15
+        var ultimaTecla = ' '
+        var score = 0
+        ScoreDisplay.setScore(0b0001_000)
+
 
 
         //spwan mostros
         //caso e tecla premida seja igual ao primeiro mostro nessa linha ilimar o mostro e aumenta o score em o valor do numero mais 1
         // mover entre linhas com '#'
-        var tempo = 10000_0000_0000L
+
         while (Pos1 > 0 && Pos2 > 0){
-                val random= (0..9).random()
-                Linah1 += digitArray[random]
-                LCD.cursor(0, Pos1)
-                LCD.write(Linah1)
-
-                LCD.cursor(1, Pos2)
-                val random2= (0..9).random()
-                Linah2 += digitArray[random2]
-                LCD.write(Linah2)
-                Pos1--
-                Pos2--
-                while (tempo != 0L)
-                {
-                        tempo -= 50
-                        var ultimaTecla = ' '
-                        val tecla = KBD.waitKey(1734552400000)
-                        if (tecla == '#') {
-                                if (ultimaTecla == Linah1[0]) {
-                                        Linah1.substring(0)
-                                }
-                                ultimaTecla = tecla
-                        }
-
+                val random= (0..8).random()
+                if (random %2 == 0) {
+                        linha0 += digitArray[random]
+                        LCD.cursor(0, Pos1)
+                        LCD.write(linha0)
+                        Pos1--
                 }
-                tempo = 10000_0000_00L
+                else {
+                        LCD.cursor(1, Pos2)
+                        linha1 += digitArray[random]
+                        LCD.write(linha1)
+                        Pos2--
+                }
+                var timePC = Time.getTimeInMillis()
+                val timeUntilSpawn = timePC + TIME_TO_SPAWN
+                while (timeUntilSpawn > timePC)
+                {
+                        cursorPos(posMain, false)
+                        val tecla = KBD.waitKey(timeUntilSpawn)
+                        if (tecla == '*') {
+                                cursorPos(posMain,true)
+                        }
+                        if (tecla == '#') {
+                                println( posMain)
+
+                                if (!posMain) {
+                                        if (ultimaTecla == linha0[0]) {
+                                                linha0 = linha0.substring(1)
+                                                Pos1++
+                                                score += ultimaTecla.toString().toInt() + 1
+                                                separarDigitos(score)
+                                        }
+                                }
+                                else{
+                                        if (ultimaTecla == linha1[0]) {
+                                                LCD.cursor(1, Pos2)
+                                                linha1 = linha1.substring(1)
+                                                LCD.write(linha1)
+                                                Pos2++
+                                                score += ultimaTecla.toString().toInt() + 1
+                                                separarDigitos(score)
+                                        }
+                                }
+                        }
+                        if(tecla != ' ') {
+                                ultimaTecla = tecla
+                                println()
+                        }
+                        timePC = Time.getTimeInMillis()
+                }
+
 
 
         }
         LCD.clear()
         LCD.write("NABOS :(")
+}
 
 
+fun cursorPos(pos: Boolean, mudar: Boolean){
+        if (mudar) {
+                if (pos) {
+                        LCD.cursor(0, 0)
+                        LCD.write(']')
+                        LCD.cursor(0, 1)
+                        posMain = false
+                        last = 0
+
+                } else {
+                        LCD.cursor(1, 0)
+                        LCD.write(']')
+                        LCD.cursor(1, 1)
+                        posMain = true
+                        last = 1
+                }
+        }
+        else{
+                LCD.cursor(0, 0)
+                LCD.write(']')
+                LCD.cursor(1, 0)
+                LCD.write(']')
+
+                LCD.cursor(last, 1)
+        }
+
+}
+fun separarDigitos(numero: Int) {
+        var num = numero
+        val digitos = mutableListOf<Int>()
+
+        while (num > 0) {
+                val digito = num % 10
+                digitos.add(0, digito)
+                num /= 10
+        }
+        updateScore(digitos.reversed())
+
+}
+
+fun updateScore(list: List<Int>){
+        for (i in 0..list.size -1){
+                ScoreDisplay.setScore(i or list[i].shl(3))//move 3 bits para a esqierda o valor e mete a pos == i, para seguir o protocolo
+        }
+        ScoreDisplay.setScore(UPTADE_SCORE)    //update
+}
+
+
+fun cleanLine(line: Int, pos: Int, size: Int){
+        var string = " "
+        for (i in 0 until size){
+                string += " "
+        }
+        LCD.cursor(line, pos)
+        LCD.write(string)
 
 
 
